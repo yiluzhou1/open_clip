d2b6c46f15806d99e12838ff0147f5b1f05b7a6f
yiluzhou1 <yzhou@globusmedical.com>
2023-11-03 09:32:32 -0400

    test

--- tests/Start_openclip_server.py
+++ tests/Start_openclip_server.py
@@ -3,3 +3,26 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run("test_fastapi:app", host="0.0.0.0", port=8001)
+
+"""
+Compiling to EXE:
+pyinstaller --noconfirm Start_openclip_server.spec
+
+a = Analysis(
+    ['Start_openclip_server.py'],
+    pathex=[],
+    binaries=[],
+    datas=[('test_fastapi.py', '.'),('static', 'static')],
+    hiddenimports=[
+        'test_fastapi',
+    ],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+)
+
+
+
+"""
